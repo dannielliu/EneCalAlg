30cccd2e6f451cca2a683090dcfae48d2727027a
danny <liudongzy@163.com>
2015-12-12 14:38:57 +0800

    modified:   src/gepep_kk.C

--- src/gepep_kk.C
+++ src/gepep_kk.C
@@ -8,6 +8,10 @@
 #include "TF1.h"
 #include <fstream>
 
+#include <stdio.h>
+#include "unistd.h"
+#include <sys/types.h>
+
 extern std::string outputdir;
 using namespace std;
 
@@ -16,7 +20,53 @@ namespace GEPEP_KK{
   void FitSpectrumPhi(TTree*& dataraw, double beame, char* namesfx, double &peak, double &peakerror);
   void FitSpecD(std::vector<Event> &evts, double beame, char* namesfx);
   void FitSpectrumD(TTree*& dataraw, double beame, char* namesfx, double &peak, double &peakerror);
+  void FitSpectrumD1(TH1D*& dataraw, double beame, char* namesfx, double &peak, double &peakerror, int id);
 }
+
+class AlgElements{
+private:
+  vector<Event> *evts;
+  TFile* file;
+  ofstream* file2;
+  TTree* result;
+  char* flag;
+  TF1*  function;
+
+  double *phipeak;
+  double *phipeake;
+  double *Dpeak;
+  double *Dpeake;
+public:
+  int id; 
+
+public:
+  AlgElements():evts(0),result(0),flag(0){}
+  ~AlgElements(){}
+  void setData(vector<Event> *vevt){ evts   = vevt;  }
+  void setOutputFile(TFile* fil)   { file   = fil;   }
+  void setOutputFile2(ofstream* fil){ file2 = fil;   }
+  void setOutputTree(TTree* res)   { result = res;   }
+  void setFlag(char* fl)           { flag   = fl;    }
+  void setFunction(TF1* func)      { function = func;}
+  void connectResult(double *mphi,double *mphie, double *mD, double *mDe)  
+  { 
+    phipeak = mphi;   
+    phipeake = mphie;   
+    Dpeak = mD;   
+    Dpeake = mDe;   
+  }
+
+  vector<Event>* getData(){ return evts;}
+  TFile* getOutputFile()  { return file;}
+  ofstream* getOutputFile2()  { return file2;}
+  TTree* getOutputTree()  { return result;}
+  char* getFlag()         { return flag;}
+  TF1* getFunction()      { return function;}
+  double *getmphi() {return phipeak;}
+  double *getmphie() {return phipeake;}
+  double *getmD() {return Dpeak;}
+  double *getmDe() {return Dpeake;}
+};
 
 void gepep_kk::Loop()
 {
@@ -126,12 +176,12 @@ void gepep_kk::Loop()
 		
 	//if (fabs(costheta1)>0.7) continue;
 	//if (fabs(costheta2)>0.7) continue;
-        if (mass>D0low && mass<D0up){
-          vars->Fill();
-        }
-        if (mass>philow && mass<phiup){
-          vars->Fill();
-        }
+      //if (mass>D0low && mass<D0up){
+      //  vars->Fill();
+      //}
+      //if (mass>philow && mass<phiup){
+      //  vars->Fill();
+      //}
         //if (costheta1 > 0.8 && costheta2 <-0.8) continue;
 	//if (p1<0.5 || p1>1.3) continue;
 	//if (p1<0.4 || p1>1.3) continue;
@@ -168,8 +218,9 @@ void gepep_kk::Loop()
    cout<<" D0: ave pK = "<<hpKD0->GetMean()<<" +/- "<<hpKD0->GetRMS()<<endl;
    cout<<"phi: ave pK = "<<hpKphi->GetMean()<<" +/- "<<hpKphi->GetRMS()<<endl;
    std::cout<<"part No: "<< Npart<<std::endl;
-   vars->Write();
+   //vars->Write();
 
+   cout<<"Prepare going into parallel process."<<endl;
    GEPEP_KK::FitSpecPhi(evtsphi[0],0,0);
 
    // ~~~~~~~~ draw end
@@ -199,84 +250,189 @@ void gepep_kk::Loop()
 #include "RooFFTConvPdf.h"
 
 using namespace RooFit;
+pthread_rwlock_t rwl = PTHREAD_RWLOCK_INITIALIZER;
+pthread_mutex_t lock_a = PTHREAD_MUTEX_INITIALIZER;
 
 double ffun(double *x, double *par)
 {
   return 1./(par[0]*pow(x[0],par[1]))+par[2];
 }
 
+void* mythread(void* par)
+{
+  AlgElements* curpar = (AlgElements*)par;
+  
+  vector<Event> &evts = *(curpar->getData());
+  TF1 &facfun = *(curpar->getFunction());
+  TTree *result = curpar->getOutputTree();
+
+
+  double philow=1.005;
+  double phiup=1.05;
+  double mphi=1.019461;// mphi
+  double D0low=1.82;
+  double D0up=1.90;
+  double mD=1.86484;// mD0
+  
+  double para = facfun.GetParameter(0);
+  double parb = facfun.GetParameter(1);
+  double parc = facfun.GetParameter(2);
+//if (pthread_rwlock_rdlock(&rwl) != 0) {
+//  perror("pthread_rwlock_rdlock");
+//  exit(-1);
+//}
+  if (pthread_mutex_lock(&lock_a) != 0) {
+    perror("lock_reader:pthread_mutex_lock");
+    exit(-1);
+  }
+  cout<<"Current parameters: "<<para << "\t" << parb<<"\t"<<parc<<endl;
+  if (pthread_mutex_unlock(&lock_a) != 0) {
+    perror("lock_reader:pthread_mutex_unlock");
+    exit(-1);
+  }
+//if (pthread_rwlock_unlock(&rwl) != 0) {
+//  perror("pthread_rwlock_unlock");
+//  exit(-1);
+//}
+  //facfun.SetParameters(para,parb,parc);
+  double mass;
+  char tname[100]; char lname[100];
+//TTree *dataraw = new TTree("dataraw","dataraw");
+//sprintf(tname,"x_%2d",curpar->id);
+//sprintf(lname,"x_%2d/D",curpar->id);
+//dataraw->Branch(tname,&mass,lname);
+//TTree *datarawf = new TTree("datarawf","dataraw");
+//sprintf(tname,"xx_%2d",curpar->id);
+//sprintf(lname,"xx_%2d/D",curpar->id);
+//datarawf->Branch(tname,&mass,lname);
+
+//dataraw->Reset();
+//datarawf->Reset();
+  
+  sprintf(tname,"hmD_%2d",curpar->id);
+  TH1D *hmD    = new TH1D(tname,"2 kaon invariant mass",100,D0low,D0up);
+  sprintf(tname,"hmphi_%2d",curpar->id);
+  TH1D *hmphi  = new TH1D(tname,"2 kaon invariant mass",100,philow,phiup);
+  hmD->Reset();
+  hmphi->Reset();
+  // D to K K
+  for (Long64_t jentry=0; jentry<evts.size();jentry++) {
+    double p1 = evts.at(jentry).GetP1();
+    double p2 = evts.at(jentry).GetP2();
+    double f1 = facfun.Eval(p1);
+    double f2 = facfun.Eval(p2);
+    mass = evts.at(jentry).InvMass(f1,f2);
+    if (mass>D0low && mass<D0up){
+      //dataraw->Fill();
+      hmD->Fill(mass);
+    }
+    if (mass>philow && mass<phiup){
+      //datarawf->Fill();
+      hmphi->Fill(mass);
+    }
+  }
+  //fitmphi=0; fitmphie=0; fitmD=0; fitmDe=0;
+  char parset[100];
+  sprintf(parset,"a%4f_b%.3f_c%.5f",para,parb,parc);
+  //char* parset = curpar.getFlag();
+  GEPEP_KK::FitSpectrumD1(hmD,mD,parset,*curpar->getmD(), *curpar->getmDe(), curpar->id);
+  //GEPEP_KK::FitSpectrumPhi(hmphi,mphi,parset,*curpar->getmphi(), *curpar->getmphie());
+//curpar->getOutputFile()->cd();
+//result->Fill();
+  delete hmD;
+  delete hmphi;
+  if (pthread_rwlock_rdlock(&rwl) != 0) {
+    perror("pthread_rwlock_rdlock");
+    exit(-1);
+  }
+  (*curpar->getOutputFile2())<< para<<"\t"<<parb<<"\t"<<parc;
+  (*curpar->getOutputFile2())<< "\t"<< *curpar->getmD() <<"\t"<<*curpar->getmDe()<<"\t"<<*curpar->getmphi()<<"\t"<<*curpar->getmphie()<<endl;
+  if (pthread_rwlock_unlock(&rwl) != 0) {
+    perror("pthread_rwlock_unlock");
+    exit(-1);
+  }
+  return NULL;
+}
+
 void GEPEP_KK::FitSpecPhi(std::vector<Event> &evts, double beame, char* namesfx)
 {
+   double para[3], parb[3], parc[3], fitmphi[3], fitmphie[3], fitmD[3], fitmDe[3];
+   TFile* files[3];
+   ofstream ofile[3];
+   TTree* result[3];
+   AlgElements parset[3];
+   for (int i=0;i<3;i++){
+     char name[100];
+     sprintf(name,"output_%02d.root",i);
+     files[i] = new TFile(name,"recreate");
+     result[i] = new TTree("result","result");
+     result[i]->Branch("para",&para[i],"para/D");
+     result[i]->Branch("parb",&parb[i],"parb/D");
+     result[i]->Branch("parc",&parc[i],"parc/D");
+     result[i]->Branch("mphi",&fitmphi[i],"mphi/D");
+     result[i]->Branch("mphie",&fitmphie[i],"mphie/D");
+     result[i]->Branch("mD",&fitmD[i],"mD/D");
+     result[i]->Branch("mDe",&fitmDe[i],"mDe/D");
 
-   double philow=1.005;
-   double phiup=1.05;
-   double mphi=1.019461;// mphi
-   double D0low=1.82;
-   double D0up=1.90;
-   double mD=1.86484;// mD0
+     sprintf(name,"output_%02d.txt",i);
+     ofile[i] = ofstream(name);
+     
+     parset[i].id = i;
+     parset[i].setData(&evts);
+     //parset[i].setOutputFile(files[i]);
+     parset[i].setOutputFile2(&ofile[i]);
+     parset[i].setOutputTree(result[i]);
+     parset[i].setFlag(namesfx);
+   }
    
-   TCanvas *c1 = new TCanvas("","",800,600);
-   double mass;
-   TTree *dataraw = new TTree("dataraw","dataraw");
-   dataraw->Branch("x",&mass,"x/D");
-   TTree *datarawf = new TTree("datarawf","dataraw");
-   datarawf->Branch("xx",&mass,"xx/D");
-   
-   double para,parb,parc, fitmphi, fitmphie, fitmD, fitmDe;
-   TTree* result = new TTree("result","result");
-   result->Branch("para",&para,"para/D");
-   result->Branch("parb",&parb,"parb/D");
-   result->Branch("parc",&parc,"parc/D");
-   result->Branch("mphi",&fitmphi,"mphi/D");
-   result->Branch("mphie",&fitmphie,"mphie/D");
-   result->Branch("mD",&fitmD,"mD/D");
-   result->Branch("mDe",&fitmDe,"mDe/D");
 
-   TF1 *facfit = new TF1("facfit",line2,0.9,1.1,2);
-   //TH1D *h1    = new TH1D("h1","2 kaon invariant mass",nBins,D0low,D0up);
+ //TF1 facfun("facfun",ffun,0,3,3);
+ //double pars[3] = {1.9e3, 2.69, -5.7e-4};
+ //facfun.SetParameters(pars);
 
-   TF1 facfun("facfun",ffun,0,3,3);
-   double pars[3] = {1.9e3, 2.69, -5.7e-4};
-   facfun.SetParameters(pars);
-
-   for (para=1000; para<=5000; para+=100){
-     for (parb=1.0; parb<=5; parb+=0.1){
-       for (parc=-2e-3; parc<2e-3; parc+=1e-4){
-         cout<<"Current parameters: "<<para << "\t" << parb<<"\t"<<parc<<endl;
-	 facfun.SetParameters(para,parb,parc);
-	 dataraw->Reset();
-	 datarawf->Reset();
-         // D to K K
-	 for (Long64_t jentry=0; jentry<evts.size();jentry++) {
-           double p1 = evts.at(jentry).GetP1();
-           double p2 = evts.at(jentry).GetP2();
-	   double f1 = facfun.Eval(p1);
-	   double f2 = facfun.Eval(p2);
-           mass = evts.at(jentry).InvMass(f1,f2);
-           if (mass>D0low && mass<D0up){
-             dataraw->Fill();
+   for (double par1=1000; par1<=1000; par1+=100){
+     for (double par2=1.0; par2<=5; par2+=0.1){
+       for (double par3=-2e-3; par3<2e-3;){
+         TF1* f1[3];
+         pthread_t tid[3];
+         void *vp[3];
+	 char name[100];
+	 for (int i=0;i<3;i++){
+           para[i] = par1;
+           parb[i] = par2;
+           parc[i] = par3;
+	   sprintf(name,"fun%02d",i);
+           f1[i] = new TF1(name,ffun,0,3,3);
+           f1[i]->SetParameters(para[i],parb[i],parc[i]);
+           parset[i].setFunction(f1[i]);
+           fitmphi[i]=0; fitmphie[i]=0; fitmD[i]=0; fitmDe[i]=0;
+	   parset[i].connectResult(&fitmphi[i],&fitmphie[i],&fitmD[i],&fitmDe[i]);
+           if (pthread_create(&tid[i], NULL, mythread, &parset[i]) != 0) {
+             perror("pthread_create");
+             exit(-1);
            }
-           if (mass>philow && mass<phiup){
-             datarawf->Fill();
+           par3 += 1e-4;
+	 }
+        
+         for (int i=0;i<3;i++)
+	 {
+	   if (pthread_join(tid[i], &vp[i]) != 0) { // wait 
+             perror("pthread_join");
+             exit(-1);
            }
+	   delete f1[i];
          }
- 	 fitmphi=0; fitmphie=0; fitmD=0; fitmDe=0;
-	 char parset[100];
-	 sprintf(parset,"a%f_b%f_c%f",para,parb,parc);
-	 GEPEP_KK::FitSpectrumD(dataraw,mD,parset,fitmD, fitmDe);
-	 GEPEP_KK::FitSpectrumPhi(datarawf,mphi,parset,fitmphi, fitmphie);
-         result->Fill();
+
        }
      }
    }
 
-   result->Write();
+   //result->Write();
    return;
 }
 
 void GEPEP_KK::FitSpectrumPhi(TTree*& dataraw, double beame, char* namesfx, double &peak, double &peakerror)
 {
-
    double philow=1.005;
    double phiup=1.05;
    double phipeak=1.019461;// mphi
@@ -377,7 +533,7 @@ void GEPEP_KK::FitSpecD(std::vector<Event> &evts, double beame, char* namesfx)
 }
 
 void GEPEP_KK::FitSpectrumD(TTree*& dataraw, double beame, char* namesfx, double &peak, double &peakerror)
-{ 
+{
    double D0low=1.82;
    double D0up=1.90;
    double peakvalue=1.86484;// mD0
@@ -466,6 +622,116 @@ void GEPEP_KK::FitSpectrumD(TTree*& dataraw, double beame, char* namesfx, double
       return;
 
 }
+void GEPEP_KK::FitSpectrumD1(TH1D*& dataraw, double beame, char* namesfx, double &peak, double &peakerror, int id)
+{  
+   double D0low=1.82;
+   double D0up=1.90;
+   double peakvalue=1.86484;// mD0
+   char name[100];
+   sprintf(name,"c_%d",id);
+   TCanvas *c1 = new TCanvas(name,name,800,600);
+   if (dataraw->GetEntries()<1) {
+     cout<< "TOO FEW EVENTS!!!!!!!!!!!"<<endl;
+     delete c1; return ;
+   }
+   if (fabs(peak)>1e-1) peakvalue = peak; 
+   
+   // try to use roofit
+   sprintf(name,"x_%d",id);
+   RooRealVar x(name,"energy",peakvalue,D0low,D0up,"GeV");
+   sprintf(name,"mean_%d",id);
+   RooRealVar mean(name,"mean of gaussian",peakvalue,D0low,D0up);
+   //RooRealVar mean2("mean2","mean of gaussian2",peakvalue,D0low,D0up);
+   //RooRealVar sigma("sigma","width of gaussian",0.0023,0.0015,0.0040);//phi version
+   //RooRealVar sigma2("sigma2","width of gaussian",0.005,0.003,0.008);
+   sprintf(name,"sigma_%d",id);
+   RooRealVar sigma(name,"width of gaussian",0.007,0.003,0.0075);//D0 version
+   sprintf(name,"sigma2_%d",id);
+   RooRealVar sigma2(name,"width of gaussian",0.02,0.008,0.05);
+   sprintf(name,"gaus_%d",id);
+   RooGaussian gaus(name,"gauss(x,m,s)",x,mean,sigma);
+   sprintf(name,"gaus2_%d",id);
+   RooGaussian gaus2(name,"gauss(x,m,s)",x,mean,sigma2);
+   
+   sprintf(name,"co1_%d",id);
+   RooRealVar co1(name,"co_1efficient #1",0,-0.5,0.5);
+   sprintf(name,"co2_%d",id);
+   RooRealVar co2(name,"co_1efficient #2",0,-0.01,0.5);
+   sprintf(name,"bkg_%d",id);
+   RooChebychev bkg(name,"background",x,RooArgList(co1,co2));
+   
+ //RooRealVar a0("a0","coefficient #0",100,100,100000);
+ //RooRealVar a1("a1","coefficient #1",-50,-100000,100000);
+ //RooPolynomial ground("ground","ground",x,RooArgList(a0,a1));
+   
+   sprintf(name,"signal_%d",id);
+   RooRealVar signal(name," ",1200,10,1000000);//event number
+   sprintf(name,"signal2_%d",id);
+   RooRealVar signal2(name," ",1200,0,1000000);//event number
+   sprintf(name,"background_%d",id);
+   RooRealVar background(name," ",200,0,1000000);
+   
+   //RooDataSet *dataset;
+   RooAddPdf *sum;
+ 
+   RooMsgService::instance().setGlobalKillBelow(RooFit::ERROR); // set out put message level of roofit
+
+   sprintf(name,"frame_%d",id);
+    RooPlot* xframe = x.frame(Title(name));
+   sprintf(name,"dataset_%d",id);
+   RooDataHist* dataset = new RooDataHist(name,name,x,dataraw);
+      char tmpchr[100];
+      sprintf(tmpchr,"data_k_%s",namesfx);
+      //data_k = new RooDataHist(tmpchr,"data_k",x,h1);
+   sprintf(name,"sum_%d",id);
+      sum = new RooAddPdf(name,"sum",RooArgList(gaus,gaus2,bkg),RooArgList(signal,signal2,background));
+      int Npar=8;
+      //mean.setVal(peakvalue+0.06*(factor-1.0));
+      sum->fitTo(*dataset,Range(D0low,D0up));
+      dataset->plotOn(xframe);
+      sum->plotOn(xframe,Components(gaus),LineStyle(2),LineColor(2));
+      sum->plotOn(xframe,Components(gaus2),LineStyle(2),LineColor(2));
+      sum->plotOn(xframe,Components(bkg),LineStyle(3),LineColor(3));
+      sum->plotOn(xframe);
+      xframe->Draw();
+      TPaveText *pt = new TPaveText(0.12,0.50,0.5,0.90,"BRNDC");
+      pt->SetBorderSize(0);
+      pt->SetFillStyle(4000);
+      pt->SetTextAlign(12);
+      pt->SetTextFont(42);
+      pt->SetTextSize(0.035);
+      sprintf(tmpchr,"#mu = %1.6f #pm %1.6f",mean.getVal(),mean.getError());
+      pt->AddText(tmpchr);
+      sprintf(tmpchr,"#sigma = %1.6f #pm %1.6f",sigma.getVal(),sigma.getError());
+      pt->AddText(tmpchr);
+      sprintf(tmpchr,"#sigma_{2} = %1.6f #pm %1.6f",sigma2.getVal(),sigma2.getError());
+      pt->AddText(tmpchr);
+      sprintf(tmpchr,"signal1 = %.2f #pm %.2f",signal.getVal(),signal.getError());
+      pt->AddText(tmpchr);
+      sprintf(tmpchr,"signal2 = %.2f #pm %.2f",signal2.getVal(),signal2.getError());
+      pt->AddText(tmpchr);
+      sprintf(tmpchr,"backNo = %.2f #pm %.2f",background.getVal(),background.getError());
+      pt->AddText(tmpchr);
+      sprintf(tmpchr,"#chi^{2}/(100-%d) = %5.6f",Npar,xframe->chiSquare(Npar));
+      pt->AddText(tmpchr);
+      //char name[100];
+      pt->Draw();
+      sprintf(name,"partD%s",namesfx);
+      c1->SetName(name);
+      c1->Write();
+      
+      
+      peak = mean.getVal();
+      peakerror = mean.getError();
+      delete dataset;
+      delete sum;
+      delete c1;
+      delete pt;
+
+      return;
+}
+
+
 
 
 
